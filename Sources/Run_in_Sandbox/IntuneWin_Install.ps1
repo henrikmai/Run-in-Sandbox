35f332dd13c1e8e22eef2931a20146f1e9b6b885
Henrik Mai <mai@softed.de>
2025-10-14 20:41:22 +0200

    customizations

--- Sources/Run_in_Sandbox/IntuneWin_Install.ps1
+++ Sources/Run_in_Sandbox/IntuneWin_Install.ps1
@@ -1,7 +1,6 @@
 param (
-    [String]$Intunewin_Content_File = "C:\Run_in_Sandbox\Intunewin_Folder.txt",
-    [String]$Intunewin_Command_File = "C:\Run_in_Sandbox\Intunewin_Install_Command.txt",
-    [switch]$OpenDebugConsole # optional: open a console after each install
+	[String]$Intunewin_Content_File = "C:\Run_in_Sandbox\Intunewin_Folder.txt",
+	[String]$Intunewin_Command_File = "C:\Run_in_Sandbox\Intunewin_Install_Command.txt"
 )
 if (-not (Test-Path $Intunewin_Content_File) ) {
 	EXIT
@@ -16,6 +15,43 @@ $Command = Get-Content -Raw $Intunewin_Command_File
 $Command = $Command.replace('"','')
 
 $FileName = (Get-Item $ScriptPath).BaseName
+
+##########################################
+# Create Folder for Intune Logs
+New-item "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs" -Force -Type Directory
+
+# Create Shortcut to Log Folder
+
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$env:PUBLIC\Desktop\Intune Logs.lnk")
+$Shortcut.TargetPath = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs"
+$Shortcut.WorkingDirectory = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs"
+$Shortcut.WindowStyle = 1
+$Shortcut.Description = "Shortcut to Intune Management Extension Logs"
+$Shortcut.Save()
+
+# Register cmtrace as Log Viewer
+# File Name Extensions
+Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -value 0
+
+$ext = ".log"
+$progId = "CMTrace.LogFile"
+$cmtracePath = "C:\Run_in_Sandbox\cmtrace.exe"
+
+# Register the file extension
+New-Item -Path "HKCU:\Software\Classes\$ext" -Force | Out-Null
+Set-ItemProperty -Path "HKCU:\Software\Classes\$ext" -Name "(Default)" -Value $progId
+
+# Register the ProgID
+New-Item -Path "HKCU:\Software\Classes\$progId\shell\open\command" -Force | Out-Null
+Set-ItemProperty -Path "HKCU:\Software\Classes\$progId" -Name "(Default)" -Value "Log File"
+Set-ItemProperty -Path "HKCU:\Software\Classes\$progId\shell\open\command" -Name "(Default)" -Value "`"$cmtracePath`" `"%1`""
+
+# Optional: Refresh Explorer to apply changes
+Stop-Process -Name explorer -Force
+
+
+###########################################
 
 $Intunewin_Extracted_Folder = "C:\Windows\Temp\intunewin"
 New-Item -Path $Intunewin_Extracted_Folder -Type Directory -Force | Out-Null
@@ -42,7 +78,4 @@ $ServiceUI = "$Sandbox_Folder\ServiceUI.exe"
 $PsExec = "$Sandbox_Folder\PsExec.exe"
 
 
-& $PsExec \\localhost -w "$Extract_Path" -nobanner -accepteula -s $ServiceUI -Process:explorer.exe C:\Windows\SysWOW64\cmd.exe /c $Command
-if ($OpenDebugConsole) {
-    & $PsExec \\localhost -w "$Extract_Path" -nobanner -accepteula -s -d $ServiceUI -Process:explorer.exe C:\Windows\SysWOW64\cmd.exe /k "echo 'Debug console for $FileName ready.'"
-}
+& $PsExec \\localhost -w "$Extract_Path" -nobanner -accepteula -s $ServiceUI -Process:explorer.exe C:\windows\SysWOW64\cmd.exe /k $Command
